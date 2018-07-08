331fac463f550f8d6d6a1042c600d0f7cc1d2275
Jason Zhu (APEX) <Jasoz@microsoft.com>
2018-07-08 16:49:18 +0800

    add first version

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+var amapFile = require('../../libs/amap-wx.js');
+var markersData = [];
+
+Page({
+  data: {
+    markers: [],
+    latitude: '',
+    longitude: '',
+    textData: {}
+  },
+  makertap: function (e) {
+    var id = e.markerId;
+    var that = this;
+    that.showMarkerInfo(markersData, id);
+    that.changeMarkerColor(markersData, id);
+  },
+  onLoad: function () {
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({ key: 'Input the Gaode developer key' });
+    myAmapFun.getPoiAround({
+      iconPathSelected: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_r.png',
+      iconPath: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png',
+      success: function (data) {
+        markersData = data.markers;
+        that.setData({
+          markers: markersData
+        });
+        that.setData({
+          latitude: markersData[0].latitude
+        });
+        that.setData({
+          longitude: markersData[0].longitude
+        });
+        that.showMarkerInfo(markersData, 0);
+      },
+      fail: function (info) {
+        wx.showModal({ title: info.errMsg })
+      }
+    })
+  },
+  showMarkerInfo: function (data, i) {
+    var that = this;
+    that.setData({
+      textData: {
+        name: data[i].name,
+        desc: data[i].address
+      }
+    });
+    if (data[i].name == "XXXX") {
+      that.playMarkerInfo();
+    }
+  },
+  changeMarkerColor: function (data, i) {
+    var that = this;
+    var markers = [];
+    for (var j = 0; j < data.length; j++) {
+      if (j == i) {
+        data[j].iconPath = "http://webapi.amap.com/theme/v1.3/markers/n/mark_r.png";
+      } else {
+        data[j].iconPath = "http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png";
+      }
+      markers.push(data[j]);
+    }
+    that.setData({
+      markers: markers
+    });
+  },
+
+  playMarkerInfo: function () {
+    const innerAudioContext = wx.createInnerAudioContext()
+    innerAudioContext.autoplay = true
+    innerAudioContext.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
+    innerAudioContext.onPlay(() => {
+      console.log('开始播放')
+    })
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
+  }
+
+})
